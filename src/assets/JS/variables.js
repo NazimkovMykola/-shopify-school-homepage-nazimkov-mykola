56e4ec1808dcf5a31d3c3066bace738702a1d91a
Mykola <148981382+NazimkovMykola@users.noreply.github.com>
2025-08-21 21:06:51 +0300

    descktop done

---
+++ src/assets/JS/variables.js
@@ -0,0 +1,19 @@
+export const commonSliderSettings = {
+  direction: "horizontal",
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  a11y: {
+    prevSlideMessage: "Previous slide",
+    nextSlideMessage: "Next slide",
+  },
+};
+
+export const colors = {
+  white: "w",
+  black: "b",
+  pink: "p",
+};
--- src/main.js
+++ src/main.js
@@ -1,18 +1,96 @@
 import Swiper from "swiper";
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/pagination";
+import { commonSliderSettings, colors } from "./assets/JS/variables";
 
-const swiper = new Swiper(".swiper-bg", {
-  direction: "horizontal",
-  loop: true,
-  speed: 1000,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  effect: "fade",
-  fadeEffect: {
-    crossFade: true,
+const swiperHero = new Swiper(".swiper-bg", {
+  modules: [Pagination, Autoplay],
+  ...commonSliderSettings,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets",
+    dynamicBullets: true,
   },
 });
 
-swiper.init();
+const swiperCollection = new Swiper(".swiper-collection", {
+  modules: [Autoplay, Navigation],
+  ...commonSliderSettings,
+  slidesPerView: 4,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: null,
+  },
+});
+
+const setLocaleStorageDialog = () => {
+  window.localStorage.setItem("isDialogViewed", "true");
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    const dialog = document.querySelector("#dialog");
+    if (dialog && window.localStorage.getItem("isDialogViewed") !== "true") {
+      dialog.showModal();
+      document.body.classList.add("no-scroll");
+    }
+    const closeDialogBtn = dialog.querySelector("#close-dialog-btn");
+    if (closeDialogBtn) {
+      closeDialogBtn.addEventListener("click", () => {
+        dialog.close();
+        document.body.classList.remove("no-scroll");
+      });
+    }
+    const btnFormSubmit = dialog.querySelector('button[type="submit"]');
+    if (btnFormSubmit) {
+      btnFormSubmit.addEventListener("click", () => {
+        setLocaleStorageDialog();
+      });
+    }
+    dialog.addEventListener("close", () => {
+      dialog.remove();
+      setLocaleStorageDialog();
+      document.body.classList.remove("no-scroll");
+    });
+  }, 1000);
+});
+
+const changeMainImage = (el, src) => {
+  el.src = src;
+};
+
+const productThumbs = document.querySelector(".product__viewer__thumbs");
+const images = productThumbs.querySelectorAll("img");
+const mainImage = document.querySelector(".product__viewer__main-img");
+images.forEach((img) => {
+  img.addEventListener("click", () => changeMainImage(mainImage, img.src));
+});
+
+const changeProductThumbs = (currentColor) => {
+  images.forEach((img, index) => {
+    img.src = `/img/product/${colors[currentColor]}${index + 1}.avif`;
+  });
+  mainImage.src = `/img/product/${colors[currentColor]}1.avif`;
+};
+const selectColorContainer = document.querySelector(
+  ".product__details__select-color"
+);
+
+if (selectColorContainer) {
+  const btns = selectColorContainer.querySelectorAll("button");
+  btns.forEach((btn, indx, btnsArr) => {
+    btn.addEventListener("click", (e) => {
+      const id = e.currentTarget.id;
+      changeProductThumbs(id);
+      btnsArr.forEach((btn) => {
+        if (btn.id !== id) {
+          btn.style.borderColor = "transparent";
+        } else {
+          btn.style.borderColor = "black";
+        }
+      });
+    });
+  });
+}
